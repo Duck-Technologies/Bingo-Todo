444e977e713fe1fcb6faedc9697a378ae17288ab
Pal Szoboszlay <szoboszlaypali@gmail.com>
2025-06-04 15:32:19 +0300

    Deployment needs resource provider to be registered

--- infra/azure/resources/container_app/main.tf
+++ infra/azure/resources/container_app/main.tf
@@ -2,6 +2,10 @@ data "azurerm_resource_group" "container_rg" {
   name = var.resource_group_name
 }
 
+resource "azurerm_resource_provider_registration" "reg" {
+  name = "Microsoft.App"
+}
+
 resource "azurerm_container_app_environment" "container_environment" {
   name                = "cae-${var.app_name}"
   location            = data.azurerm_resource_group.container_rg.location
@@ -10,7 +14,8 @@ resource "azurerm_container_app_environment" "container_environment" {
   tags = var.tags
 
   depends_on = [
-    data.azurerm_resource_group.container_rg
+    data.azurerm_resource_group.container_rg,
+    azurerm_resource_provider_registration.reg
   ]
 }
 
