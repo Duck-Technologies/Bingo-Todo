ec7bd5d7dcc4aa44204ae6f2ca9857da769b6d55
Pal Szoboszlay <szoboszlaypali@gmail.com>
2025-07-02 14:27:35 +0300

    Fix container reference

--- infra/azure/resources/container_app/main.tf
+++ infra/azure/resources/container_app/main.tf
@@ -131,7 +131,7 @@ resource "azurerm_container_app" "container_app" {
 
   lifecycle {
     ignore_changes = [
-      template[0].container.image,
+      template[0].container[0].image,
     ]
   }
 
