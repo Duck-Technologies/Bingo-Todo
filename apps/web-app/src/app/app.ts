a127203bfa3672e29d5b8ca778bf5d41c08f7737
szoboszlaypali <155780731+szoboszlaypali@users.noreply.github.com>
2025-07-07 17:22:03 +0300

    Included Material 3 with default light-dark theme for the site (#24)

--- apps/web-app/src/app/app.ts
+++ apps/web-app/src/app/app.ts
@@ -3,12 +3,24 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { User } from './core/auth/user';
 import { AsyncPipe } from '@angular/common';
 import { AuthOperations } from './core/auth/auth-operations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, AsyncPipe],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    AsyncPipe,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIcon
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   private userProvider = inject(User);
